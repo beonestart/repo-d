010d1c90eae932273a5f367af46ae21b53647ca6
Startapp-pro <103786043+Startapp-pro@users.noreply.github.com>
2022-05-31 11:44:56 +0100

    first commit

---
+++ js/consts-vars.js
@@ -0,0 +1,12 @@
+﻿
+const dataConfig = async () => fetch("/auth_config.json");
+
+
+window.onload = async () => {
+    const params = await response.json();
+    const app_url = params.dev_urls;
+
+    export const apiBaseUrl = api_url.api_url;
+    export const idsBaseUrl = app_url.ids_url;
+    export const spaBaseUrl = app_url.spa_url;
+};
---
+++ js/your-script.js
@@ -0,0 +1,2 @@
+﻿
+//add your script here
